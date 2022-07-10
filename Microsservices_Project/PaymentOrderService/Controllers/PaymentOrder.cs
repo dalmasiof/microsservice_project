eff76068c3f68345a300059d420a46e022cd666e
Dalmasio Fernandes <dalmasiof@gmail.com>
2022-07-10 20:22:23 -0300

    PoSvc controllers

--- Microsservices_Project/PaymentOrderService/Controllers/PaymentOrder.cs
+++ Microsservices_Project/PaymentOrderService/Controllers/PaymentOrder.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PaymentOrderService.Service;
+using PaymentOrderService.ViewModels;
 
 namespace PaymentOrderService.Controllers
 {
@@ -7,5 +9,46 @@ namespace PaymentOrderService.Controllers
     [ApiController]
     public class PaymentOrderController : ControllerBase
     {
+        private readonly IPOService _pOServicxe;
+
+        public PaymentOrderController(IPOService pOServicxe)
+        {
+            _pOServicxe = pOServicxe;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            var userList = await _pOServicxe.Get();
+            return Ok(userList);
+        }
+
+        [HttpGet("{Id}")]
+        public async Task<ActionResult> Get(long Id)
+        {
+            var userList = await _pOServicxe.Get(Id);
+            return Ok(userList);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(PoVm poVm)
+        {
+            return Ok(await _pOServicxe.Create(poVm));
+        }
+
+        [HttpDelete("{Id}")]
+        public async Task<ActionResult> Delete(int Id)
+        {
+            if (await _pOServicxe.Delete(Id))
+                return Ok();
+            else
+                return BadRequest();
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Put(PoVm poVm)
+        {
+            return Ok(await _pOServicxe.Update(poVm));
+        }
     }
 }
